400b1d3fa70ca25a1dd76554ed354ec67ff77dff
Aniket <aniketsingh2151@gmail.com>
2025-11-25 21:20:35 +0530

    project init

---
+++ src/test/java/com/promptcraft/promptcraft/PromptcraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promptcraft.promptcraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptcraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
